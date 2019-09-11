bd6a146c99fd22a18a5674acbe268e6ca45fdeea
boredazfcuk <boredazfcuk@hotmail.co.uk>
2019-09-11 16:07:33 +0100

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,46 @@
+FROM alpine:latest
+MAINTAINER boredazfcuk
+ENV APPBASE="/Deluge" \
+   BUILDDEPENDENCIES="curl gcc geoip-dev openssl-dev libffi-dev musl-dev python3-dev zlib-dev jpeg-dev" \
+   RUNTIMEDEPENDENCIES="tzdata py3-six py3-openssl geoip git" \
+   NZBTOMEDIADEPENDENCIES="python3" \
+   CONFIGDIR="/config" \
+   N2MBASE="/nzbToMedia" \
+   N2MREPO="clinton-hall/nzbToMedia" \
+   PYTHON_EGG_CACHE="/config/.pythoneggcache" \
+   LOGDIR="/tmp" \
+   LOG_DAEMON="deluge-daemon.log" \
+   LOG_WEB="deluge-web.log"
+
+COPY start-deluge.sh /usr/local/bin/start-deluge.sh
+
+RUN echo "$(date '+%d/%m/%Y - %H:%M:%S') | ***** BUILD STARTED *****" && \
+echo "$(date '+%d/%m/%Y - %H:%M:%S') | Add group, user and directories" && \
+   mkdir -p "${APPBASE}" "${N2MBASE}" && \
+echo "$(date '+%d/%m/%Y - %H:%M:%S') | Install build dependencies" && \
+   apk add --no-cache --no-progress --virtual=build-deps ${BUILDDEPENDENCIES} && \
+   apk --repository "http://dl-cdn.alpinelinux.org/alpine/edge/testing" add --no-cache --no-progress py3-libtorrent-rasterbar && \
+echo "$(date '+%d/%m/%Y - %H:%M:%S') | Install Deluge" && \
+   pip3 install --no-cache-dir --upgrade pip && \
+   pip3 install --no-cache-dir deluge geoip && \
+echo "$(date '+%d/%m/%Y - %H:%M:%S') | Install Deluge runtime dependencies" && \
+   apk add --no-cache --no-progress ${RUNTIMEDEPENDENCIES} && \
+echo "$(date '+%d/%m/%Y - %H:%M:%S') | Install ${N2MREPO}" && \
+   cd "${N2MBASE}" && \
+   git clone -b master "https://github.com/${N2MREPO}.git" "${N2MBASE}" && \
+   mkdir /shared && \
+   touch "/shared/autoProcessMedia.cfg" && \
+   ln -s "/shared/autoProcessMedia.cfg" "${N2MBASE}/autoProcessMedia.cfg" && \
+echo "$(date '+%d/%m/%Y - %H:%M:%S') | Set permissions on launcher script" && \
+   chmod +x /usr/local/bin/start-deluge.sh && \
+echo "$(date '+%d/%m/%Y - %H:%M:%S') | Install Clean Up" && \
+   apk del --purge --no-progress build-deps && \
+   rm -rfv "/shared" ~/.pip/cache/* ~/.cache && \
+echo "$(date '+%d/%m/%Y - %H:%M:%S') | ***** BUILD COMPLETE *****"
+
+HEALTHCHECK --start-period=10s --interval=1m --timeout=10s \
+   CMD wget --quiet --tries=1 --spider http://${HOSTNAME}:8112/ || exit 1
+
+VOLUME "${CONFIGDIR}"
+
+CMD /usr/local/bin/start-deluge.sh
---
+++ start-deluge.sh
@@ -0,0 +1,68 @@
+#!/bin/ash
+
+##### Functions #####
+Initialise(){
+   PAD="    "
+   PID=$$
+   PID="${PID:0:4}${PAD:0:$((4 - ${#PID}))}"
+   echo "$(date '+%H:%M:%S') [INFO    ][deluge.launcher.docker        :${PID}] ***** Starting Deluge *****"
+   if [ ! -d "${PYTHON_EGG_CACHE}" ]; then mkdir "${PYTHON_EGG_CACHE}"; fi
+   if [ -f "${LOGDIR}/${LOG_DAEMON}" ]; then rm "${LOGDIR}/${LOG_DAEMON}"; ln -s /dev/stdout "${LOGDIR}/${LOG_DAEMON}"; fi
+   if [ -f "${LOGDIR}/${LOG_WEB}" ]; then rm "${LOGDIR}/${LOG_WEB}"; ln -s /dev/stdout "${LOGDIR}/${LOG_WEB}"; fi
+   if [ -z "${USER}" ]; then echo "$(date '+%H:%M:%S') [WARNING ][deluge.launcher.docker        :${PID}] User name not set, defaulting to 'user'"; USER="user"; fi
+   if [ -z "${UID}" ]; then echo "$(date '+%H:%M:%S') [WARNING ][deluge.launcher.docker        :${PID}] User ID not set, defaulting to '1000'"; UID="1000"; fi
+   if [ -z "${GROUP}" ]; then echo "$(date '+%H:%M:%S') [WARNING ][deluge.launcher.docker        :${PID}] Group name not set, defaulting to 'group'"; GROUP="group"; fi
+   if [ -z "${GID}" ]; then echo "$(date '+%H:%M:%S') [WARNING ][deluge.launcher.docker        :${PID}] Group ID not set, defaulting to '1000'"; GID="1000"; fi
+   echo "$(date '+%H:%M:%S') [INFO    ][deluge.launcher.docker        :${PID}] Local user: ${USER}:${UID}"
+   echo "$(date '+%H:%M:%S') [INFO    ][deluge.launcher.docker        :${PID}] Local group: ${GROUP}:${GID}"
+   echo "$(date '+%H:%M:%S') [INFO    ][deluge.launcher.docker        :${PID}] Deluge application directory: ${APPBASE}"
+}
+
+CreateGroup(){
+   if [ -z "$(getent group "${GROUP}" | cut -d: -f3)" ]; then
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Group ID available, creating group"
+      addgroup -g "${GID}" "${GROUP}"
+   elif [ ! "$(getent group "${GROUP}" | cut -d: -f3)" = "${GID}" ]; then
+      echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR:   Group GID mismatch - exiting"
+      exit 1
+   fi
+}
+
+CreateUser(){
+   if [ -z "$(getent passwd "${USER}" | cut -d: -f3)" ]; then
+      echo "$(date '+%H:%M:%S') [INFO    ][deluge.launcher.docker        :${PID}] User ID available, creating user"
+      adduser -s /bin/ash -H -D -G "${GROUP}" -u "${UID}" "${USER}"
+   elif [ ! "$(getent passwd "${USER}" | cut -d: -f3)" = "${UID}" ]; then
+      echo "$(date '+%H:%M:%S') [INFO    ][deluge.launcher.docker        :${PID}] User ID already in use - exiting"
+      exit 1
+   fi
+}
+
+SetOwnerAndGroup(){
+   echo "$(date '+%H:%M:%S') [INFO    ][deluge.launcher.docker        :${PID}] Correct owner and group of application files, if required"
+   find "${APPBASE}" ! -user "${USER}" -exec chown "${USER}" {} \;
+   find "${APPBASE}" ! -group "${GROUP}" -exec chgrp "${GROUP}" {} \;
+   find "${N2MBASE}" ! -user "${USER}" -exec chown "${USER}" {} \;
+   find "${N2MBASE}" ! -group "${GROUP}" -exec chgrp "${GROUP}" {} \;
+   find "${PYTHON_EGG_CACHE}" ! -user "${USER}" -exec chown "${USER}" {} \;
+   find "${PYTHON_EGG_CACHE}" ! -group "${GROUP}" -exec chgrp "${GROUP}" {} \;
+   find "${CONFIGDIR}" ! -user "${USER}" -exec chown "${USER}" {} \;
+   find "${CONFIGDIR}" ! -group "${GROUP}" -exec chgrp "${GROUP}" {} \;
+}
+
+LaunchDeluge(){
+   echo "$(date '+%H:%M:%S') [INFO    ][deluge.launcher.docker        :${PID}] Starting Deluge as ${USER}"
+   su -m "${USER}" -c '/usr/bin/deluged -c '"${CONFIGDIR}"' -L info -l '"${LOGDIR}/${LOG_DAEMON}"''
+   echo "$(date '+%H:%M:%S') [INFO    ][deluge.launcher.docker        :${PID}] Start Deluge webui"
+   su -m "${USER}" -c '/usr/bin/deluge-web -c '"${CONFIGDIR}"' -L error -l '"${LOGDIR}/${LOG_WEB}"''
+   sleep 999999h
+   echo "$(date '+%H:%M:%S') [INFO    ][deluge.launcher.docker        :${PID}] ***** Stopping Deluge *****"
+}
+
+##### Script #####
+Initialise
+CreateGroup
+CreateUser
+LaunchDeluge
+
+
