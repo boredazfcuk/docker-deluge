1b3c6c204faeb4a6fa81aa2f769621b9e8f41fc2
boredazfcuk <boredazfcuk@hotmail.co.uk>
2019-10-21 21:54:29 +0100

    Healthcheck fix

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,9 +1,5 @@
 #!/bin/ash
-
 DELUGECREDENTIALS="$(grep ^${USER} /config/auth | cut -d: -f1-2 | tr : ' ')"
-
-/usr/bin/deluge-console "connect localhost ${DELUGECREDENTIALS}" 2>&1 | wc -c | grep -qw 0 || echo "Deluge Daemon not reachable on port 8112"; exit 1
-
-wget -qSO /dev/null "http://${HOSTNAME}:8112/" || echo "Deluge Web not reachable on port 8112"; exit 1
-
+/usr/bin/deluge-console "connect localhost ${DELUGECREDENTIALS}" >/dev/null 2>&1 | wc -c | grep -wq 0 || exit 1
+wget -q --spider "http://${HOSTNAME}:8112/" || exit 1
 exit 0
