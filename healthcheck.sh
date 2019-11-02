5e1d9a991c935cda9347e78803feeb2eb1bd1814
boredazfcuk <boredazfcuk@hotmail.co.uk>
2019-11-02 18:48:45 +0000

    Manual push of any changes to stack containers

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,5 +1,5 @@
 #!/bin/ash
 DELUGECREDENTIALS="$(grep ^${USER} /config/auth | cut -d: -f1-2 | tr : ' ')"
 /usr/bin/deluge-console "connect localhost ${DELUGECREDENTIALS}" >/dev/null 2>&1 | wc -c | grep -wq 0 || exit 1
-wget -q --spider "http://${HOSTNAME}:8112/" || exit 1
+wget --quiet --tries=1 --no-check-certificate --spider "https://${HOSTNAME}:8112/" || exit 1
 exit 0
