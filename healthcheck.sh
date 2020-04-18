2a1b1206430b356423776eabb5032c0a383fb768
boredazfcuk <boredazfcuk@hotmail.co.uk>
2020-04-18 23:16:06 +0100

    Please check change.log

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,9 +1,9 @@
 #!/bin/ash
-if [ "$(nc -z localhost 58846; echo $?)" -ne 0 ]; then
+if [ "$(netstat -plnt | grep -c 58846)" -ne 1 ]; then
    echo "Deluge daemon not responding on port 58846"
    exit 1
 fi
-if [ "$(nc -z "$(hostname -i)" 8112; echo $?)" -ne 0 ]; then
+if [ "$(netstat -plnt | grep -c 8112)" -ne 1 ]; then
    echo "Deluge WebUI not responding on port 8112"
    exit 1
 fi
