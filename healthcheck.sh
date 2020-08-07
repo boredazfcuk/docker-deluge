be49ec529c7b23df1806407a74279331358f8c0c
boredazfcuk <boredazfcuk@hotmail.co.uk>
2020-08-07 11:33:07 +0100

    Please check change.log

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,11 +1,19 @@
 #!/bin/ash
+
 if [ "$(netstat -plnt | grep -c 58846)" -ne 1 ]; then
    echo "Deluge daemon not responding on port 58846"
    exit 1
 fi
+
 if [ "$(netstat -plnt | grep -c 8112)" -ne 1 ]; then
    echo "Deluge WebUI not responding on port 8112"
    exit 1
 fi
+
+if [ "$(ip -o addr | grep "$(hostname -i)" | wc -l)" -eq 0 ]; then
+   echo "NIC missing"
+   exit 1
+fi
+
 echo "Deluge daemon and WebUI responding OK"
 exit 0
